0551b12fe8d026f604ee21205817bed6aca65139
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-24 20:31:01 +0100

    changed name on performance-data

--- src/data/work.ts
+++ src/data/work.ts
@@ -1,4 +1,4 @@
-interface Performance {
+interface Work {
   id: string;
   name: string;
   role: string;
@@ -6,7 +6,7 @@ interface Performance {
   year: number;
 }
 
-export const mockedPerformance: Performance[] = [
+export const mockedPerformance: Work[] = [
   {
     id: "1",
     name: "Miss Saigon",
@@ -16,30 +16,30 @@ export const mockedPerformance: Performance[] = [
   },
   {
     id: "2",
-    name: "Miss Saigon",
-    role: "",
-    workplace: "",
+    name: "Grease",
+    role: "Danny",
+    workplace: "Kungälv",
     year: 2015,
   },
   {
     id: "3",
     name: "Miss Saigon",
-    role: "",
-    workplace: "",
+    role: "cvnc",
+    workplace: "bmvnfn",
     year: 2015,
   },
   {
     id: "4",
     name: "Miss Saigon",
-    role: "",
-    workplace: "",
+    role: "vnc",
+    workplace: "cvncn",
     year: 2015,
   },
   {
     id: "5",
     name: "Miss Saigon",
-    role: "",
-    workplace: "",
+    role: "cvncn",
+    workplace: "cnvcn",
     year: 2015,
   },
 ];
