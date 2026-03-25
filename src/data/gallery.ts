be37d84ded31df30709d26c6955bd738f90c4ecd
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-25 15:05:26 +0100

    add more images

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -3,25 +3,33 @@ import hero from "../assets/hero.png";
 import ikea2 from "../assets/ikea2.png";
 import ikea3 from "../assets/ikea3.png";
 import ikea4 from "../assets/ikea4.png";
+import karlek from "../assets/karlek.jpg";
 import lejonhjarta from "../assets/lejonhjarta.png";
+import malmo from "../assets/malmo.jpg";
 import oliver from "../assets/oliver-musical.png";
 import profil from "../assets/profil.jpeg";
 import profil2 from "../assets/profil2.png";
 import profil3 from "../assets/profil3.png";
+import rotten from "../assets/rotten.jpg";
 import svala from "../assets/svala.png";
 import young from "../assets/young-markus.png";
+import zjivago from "../assets/zjivago.jpg";
 
-export const stageImages = [
-  { src: grandhotel, name: "grandhotel" },
-  { src: hero, name: "hero" },
-  { src: ikea2, name: "ikea2" },
-  { src: ikea3, name: "ikea3" },
-  { src: ikea4, name: "ikea4" },
-  { src: lejonhjarta, name: "lejonhjarta" },
-  { src: oliver, name: "oliver" },
-  { src: profil, name: "profil" },
-  { src: profil2, name: "profil2" },
-  { src: profil3, name: "profil3" },
-  { src: svala, name: "svala" },
-  { src: young, name: "young" },
-];
+export const stageImages = {
+  grandhotel,
+  hero,
+  ikea2,
+  ikea3,
+  ikea4,
+  karlek,
+  lejonhjarta,
+  oliver,
+  profil,
+  profil2,
+  profil3,
+  svala,
+  young,
+  malmo,
+  zjivago,
+  rotten,
+};
--- src/data/work.ts
+++ src/data/work.ts
@@ -1,10 +1,12 @@
+import { stageImages } from "./gallery";
+
 export interface Work {
   id: string;
   name: string;
   role: string;
   imageUrl: string;
   workplace: string;
-  year: number;
+  year: string;
 }
 
 export const workData: Work[] = [
@@ -12,57 +14,57 @@ export const workData: Work[] = [
     id: "1",
     name: "Det var en gång på Grand Hotel",
     role: "Kvartett/Storfursten",
-    imageUrl: "",
+    imageUrl: stageImages.grandhotel,
     workplace: "Göteborgsoperan",
-    year: 2023,
+    year: "2023",
   },
   {
     id: "2",
     name: "Kärlek skonar ingen",
     role: "US Tommy/Ensemble",
-    imageUrl: "",
+    imageUrl: stageImages.karlek,
     workplace: "Göteborgsoperan",
-    year: 2022,
+    year: "2021-2022",
   },
   {
     id: "3",
     name: "Oliver!",
     role: "Swing",
-    imageUrl: "",
+    imageUrl: stageImages.oliver,
     workplace: "Göteborgsoperan",
-    year: 2020,
+    year: "2019-2020",
   },
   {
     id: "4",
     name: "Something Rotten",
     role: "US Nigel & Shakespeare",
-    imageUrl: "",
+    imageUrl: stageImages.rotten,
     workplace: "Wermland Opera",
-    year: 2019,
+    year: "2018-2019",
   },
   {
     id: "5",
     name: "Tummelisa",
     role: "Svala",
-    imageUrl: "",
+    imageUrl: stageImages.svala,
     workplace: "Örebro Länsteater",
-    year: 2018,
+    year: "2017-2018",
   },
   {
     id: "6",
     name: "Bröderna Lejonhjärta",
     role: "Jonatan",
-    imageUrl: "",
+    imageUrl: stageImages.lejonhjarta,
     workplace: "Kristianstad Teater",
-    year: 2016,
+    year: "2016",
   },
   {
     id: "7",
     name: "Ingvar, En musikalisk möbelsaga",
     role: "Matteus",
-    imageUrl: "",
+    imageUrl: stageImages.ikea2,
     workplace: "Teater Västernorrland",
-    year: 2016,
+    year: "2016",
   },
   {
     id: "8",
@@ -70,22 +72,22 @@ export const workData: Work[] = [
     role: "Kapellmästare, pianist",
     imageUrl: "",
     workplace: "Lilla Beddinge Teater",
-    year: 2016,
+    year: "2016",
   },
   {
     id: "9",
-    name: "Dr. Zhivago",
+    name: "Doktor Zjivago",
     role: "Ensemble",
-    imageUrl: "",
+    imageUrl: stageImages.zjivago,
     workplace: "Malmö Opera",
-    year: 2015,
+    year: "2014-2015",
   },
   {
     id: "10",
     name: "Miss Saigon",
     role: "Ensemble",
-    imageUrl: "",
+    imageUrl: stageImages.malmo,
     workplace: "Malmö Opera",
-    year: 2014,
+    year: "2013-2014",
   },
 ];
