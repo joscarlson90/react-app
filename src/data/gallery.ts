819e1814eb022598ca66510eb6213a412670ae34
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-25 16:23:20 +0100

    add more images

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -2,34 +2,30 @@ import grandhotel from "../assets/grandhotel.webp";
 import hero from "../assets/hero.png";
 import ikea2 from "../assets/ikea2.png";
 import ikea3 from "../assets/ikea3.png";
-import ikea4 from "../assets/ikea4.png";
 import karlek from "../assets/karlek.jpg";
-import lejonhjarta from "../assets/lejonhjarta.png";
+import lejonhjarta from "../assets/lejonhjarta2.avif";
 import malmo from "../assets/malmo.jpg";
 import oliver from "../assets/oliver-musical.png";
 import profil from "../assets/profil.jpeg";
 import profil2 from "../assets/profil2.png";
-import profil3 from "../assets/profil3.png";
-import rotten from "../assets/rotten.jpg";
+import rotten from "../assets/somethingrotten.avif";
 import svala from "../assets/svala.png";
 import young from "../assets/young-markus.png";
 import zjivago from "../assets/zjivago.jpg";
 
 export const stageImages = {
-  grandhotel,
-  hero,
-  ikea2,
-  ikea3,
-  ikea4,
-  karlek,
-  lejonhjarta,
-  oliver,
-  profil,
-  profil2,
-  profil3,
-  svala,
-  young,
-  malmo,
-  zjivago,
-  rotten,
+  grandhotel: {src: grandhotel, inGallery: true},
+  hero: {src: hero, inGallery: false},
+  ikea2: {src: ikea2, inGallery: true},
+  ikea3: {src: ikea3, inGallery: true},
+  karlek: {src: karlek, inGallery: true},
+  lejonhjarta: {src: lejonhjarta, inGallery: true},
+  oliver: {src: oliver, inGallery: true},
+  profil: {src: profil, inGallery: false},
+  profil2: {src: profil2, inGallery: false},
+  svala: {src: svala, inGallery: true},
+  young: {src: young, inGallery: false},
+  malmo: {src: malmo, inGallery: true},
+  zjivago: {src: zjivago, inGallery: true},
+  rotten: {src: rotten, inGallery: true},
 };
