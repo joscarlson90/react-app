c29eee3f9a8bcab88c740c73ac89a13d861fea28
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-25 20:11:09 +0100

    change styling

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       className="bg-[length:40%] bg-no-repeat h-screen w-full bg-right flex items-center justify-around"
       style={{ backgroundImage: `url(${heroImg.src})` }}
     >
-      <h2 className="text-3xl font-thin italic">Sångare & Skådespelare</h2>
+      <h2 className="text-3xl font-thin">Sångare & Skådespelare</h2>
       <div></div>
     </section>
   );
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -5,11 +5,11 @@ export default function Work() {
 
   return (
     <div className="px-6 py-10">
-      <p className="text-xl font-thin ml-10 mt-10">SCEN</p>
+      <p className="text-xl ml-10 mt-10">SCEN</p>
 
       <div className="flex flex-col">
         {work.map((item) => (
-          <div className="grid grid-cols-3 my-6 ml-10" key={item.id}>
+          <div className="grid grid-cols-3 my-6 ml-10 font-thin" key={item.id}>
             <p className="text-2xl">{item.year}</p>
             <div className="flex flex-col">
               <p className="text-3xl mb-3">{item.workplace}</p>
