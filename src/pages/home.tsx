0cb3df2db93d233ad102ed5bbb276419b29716af
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-28 22:27:51 +0100

    change styling for media query

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,14 +4,17 @@ export default function Home() {
   const heroImg = stageImages.hero;
 
   return (
-    <section className="flex flex-col items-center justify-center w-full tablet:px-8 md:flex-row md:items-center md:justify-between">
-      <article className="hidden md:flex flex-col justify-center w-full md:w-1/2 lg:w-2/5">
-        <h2 className="text-3xl font-thin md:text-4xl md:text-center">
+    <section className="flex flex-col items-center justify-center w-full tablet:px-8 md:flex-row md:items-center md:justify-right">
+      <article className="hidden md:absolute md:inset-0 md:flex flex-col justify-center items-center w-full md:w-auto md:translate-x-[-20%]">
+        <h2 className="mt-50 mb-5 text-2xl font-thin md:text-4xl">
           Sångare & Skådespelare
         </h2>
+        <h3 className="w-auto font-light">
+          hejhej
+        </h3>
       </article>
 
-      <div className="w-full md:w-1/2 lg:w-2/5">
+      <div className="w-full md:w-1/2 lg:w-2/5 md:ml-auto">
         <img
           src={heroImg.src}
           alt="Scenbild"
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -3,10 +3,10 @@ import { Link, Outlet } from "react-router";
 export default function Layout() {
   return (
     <>
-      <header className="grid font-thin ">
+      <header className="font-thin z-10 relative">
         <div className="flex flex-col items-center justify-between text-4xl p-5 pl-10 border-b-3 border-red-800 bg-stone-200 md:justify-between">
           <h1 className="text">Markus Christensen</h1>
-          <h2 className="m-2 block md:hidden text-xl">
+          <h2 className=" block md:hidden text-xl">
             Sångare & Skådespelare
           </h2>
           <div className="flex flex-row flex-wrap text-base gap-5 p-4">
@@ -21,7 +21,7 @@ export default function Layout() {
       <Outlet />
       <footer className="font-thin">
         <div className="flex items-center justify-center h-30 text-sm gap-5 p-5 border-t-3 border-red-800 bg-stone-200">
-          <div className="text-center">© Josefine Carlson</div>
+          <div className="text-center">© 2026 Markus Christensen</div>
         </div>
       </footer>
     </>
