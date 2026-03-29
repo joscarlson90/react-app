17b4689246f39c693f78b9c5eee8a1052a9a4c71
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-29 20:50:25 +0200

    add about me text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,21 +13,24 @@ export default function About() {
           className="rounded-md float-right w-100 m-3"
         />
         <p className="">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi maxime
-          ullam quasi ipsum facilis provident, praesentium doloremque modi
-          asperiores? A, est? Suscipit, dolor fuga ipsum omnis at velit culpa
-          vel? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi
-          maxime ullam quasi ipsum facilis provident, praesentium doloremque
-          modi asperiores? A, est? Suscipit, dolor fuga ipsum omnis at velit
-          culpa vel? Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Sequi maxime ullam quasi ipsum facilis provident, praesentium
-          doloremque modi asperiores? A, est? Suscipit, dolor fuga ipsum omnis
-          at velit culpa vel? Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Sequi maxime ullam quasi ipsum facilis provident,
-          praesentium doloremque modi asperiores? A, est? Suscipit, dolor fuga
-          ipsum omnis at velit culpa vel? Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Sequi maxime ullam quasi ipsum facilis
-          provident, praesentium doloremque modi asperiores?
+          - Jag vill bli Spiderman när jag blir stor! <br />
+          Andrahandsval tennisproffs. Det blir inte alltid som man tänkt sig.
+          När jag upptäckte musiken - alltså musik som var mer än Smurfhits och
+          Markoolio - skiftade mitt fokus. Jag började på Ytterbyskolans
+          musikklasser och förstod att det var basist i Så ska det låta-bandet
+          som var mitt kall i livet. Musiken tog mig vidare till
+          musikalprogrammet på Mimers Hus gymnasium i Kungälv och vidare till
+          Musikteaterskolan i Bjärnum, Balettakademien i Göteborg och slutligen
+          Högskolan för Scen och Musik.
+          <br />
+          Det blev varken en framtid som Spindelman eller Så ska det låta-basist
+          men däremot musikalartist på scener runt om i Sverige, skådespelare på
+          tv och mängder av sånguppdrag vid bröllop, begravningar och fester.
+          <br />
+          Med bredden från att spela i coverband på Kungälvs allra bästa kroger
+          (ja, jag sa det - Kungälvs bästa krogar) till att spela
+          lustspelsoperett på Göteborgsoperan kan jag med stolthet definiera mig
+          som mångsysslare inom musik och teater.
         </p>
         {/* <p className="h-auto w-auto md:w-1/2 lg:w-2/5 md:ml-auto"></p> */}
       </section>
