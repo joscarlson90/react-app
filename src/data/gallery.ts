f5996fb25627e57fad05a78aebb1fa8f1a494a5e
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-29 15:07:59 +0200

    add more images etc

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -4,19 +4,20 @@ import ikea2 from "../assets/ikea2.png";
 import ikea3 from "../assets/ikea3.png";
 import karlek from "../assets/karlek.jpg";
 import lejonhjarta from "../assets/lejonhjarta2.avif";
+import loveyou from "../assets/loveyou.png";
 import malmo from "../assets/malmo.jpg";
 import oliver from "../assets/oliver-musical.png";
 import oliver2 from "../assets/oliver2.jpg";
 import profil from "../assets/profil.jpeg";
 import profil2 from "../assets/profil2.png";
 import rotten from "../assets/somethingrotten.avif";
+import somethingrotten from "../assets/somethingrotten2.avif";
+import somethingrotten3 from "../assets/somethingrotten3.avif";
+import somethingrotten4 from "../assets/somethingrotten4.avif";
 import svala from "../assets/svala.png";
 import tummelisa from "../assets/tummelisa.webp";
 import young from "../assets/young-markus.png";
 import zjivago from "../assets/zjivago.jpg";
-import somethingrotten from "../assets/somethingrotten2.avif";
-import somethingrotten3 from "../assets/somethingrotten3.avif";
-import somethingrotten4 from "../assets/somethingrotten4.avif";
 
 export const stageImages = {
   grandhotel: { src: grandhotel, inGallery: true },
@@ -38,4 +39,5 @@ export const stageImages = {
   somethingrotten: { src: somethingrotten, inGallery: true },
   somethingrotten3: { src: somethingrotten3, inGallery: true },
   somethingrotten4: { src: somethingrotten4, inGallery: true },
+  loveyou: { src: loveyou, inGallery: true },
 };
