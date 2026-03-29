0bf1a138ff38b3efccb076115bf848835a8c120a
joscarlson90 <josefinecarlson90@gmail.com>
2026-03-29 15:08:25 +0200

    change about in each work

--- src/data/work.ts
+++ src/data/work.ts
@@ -7,6 +7,7 @@ export interface Work {
   imageUrl: string;
   workplace: string;
   year: string;
+  about: string;
 }
 
 export const workData: Work[] = [
@@ -17,6 +18,8 @@ export const workData: Work[] = [
     imageUrl: stageImages.grandhotel.src,
     workplace: "Göteborgsoperan",
     year: "2023",
+    about:
+      "Lustspelsoperett i två akter av Paul Abraham (1892–1960). Libretto Alfred Grünwald och Fritz Löhner-Beda efter en pjäs av Alfred Savoir. Ny svensk översättning av Fredrik Fischer och Linnea Sjunnesson.",
   },
   {
     id: "2",
@@ -25,6 +28,8 @@ export const workData: Work[] = [
     imageUrl: stageImages.karlek.src,
     workplace: "Göteborgsoperan",
     year: "2021-2022",
+    about:
+      "Ett poetiskt och skitigt drömspel som utspelar sig på platser som Håkan Hellström för alltid skrivit in i Göteborgs stadskarta.",
   },
   {
     id: "3",
@@ -33,6 +38,8 @@ export const workData: Work[] = [
     imageUrl: stageImages.oliver.src,
     workplace: "Göteborgsoperan",
     year: "2019-2020",
+    about:
+      "Fredrik Benke Rydmans version av den klassiska Dickens-musikalen om Oliver Twist.",
   },
   {
     id: "4",
@@ -41,6 +48,8 @@ export const workData: Work[] = [
     imageUrl: stageImages.rotten.src,
     workplace: "Wermland Opera",
     year: "2018-2019",
+    about:
+      "Produktionen blev en härligt galen hyllning, drift och kärleksförklaring till allt vad musikal, broadway och Shakespeare är!",
   },
   {
     id: "5",
@@ -49,6 +58,8 @@ export const workData: Work[] = [
     imageUrl: stageImages.svala.src,
     workplace: "Örebro Länsteater",
     year: "2017-2018",
+    about:
+      "Föreställningen, baserad på H.C. Andersens klassiska saga, fokuserade på mod och att ta makten över sitt eget liv. Den beskrevs som en generös musikal i stort format med färgstarka karaktärer och specialskriven musik",
   },
   {
     id: "6",
@@ -57,6 +68,8 @@ export const workData: Work[] = [
     imageUrl: stageImages.lejonhjarta.src,
     workplace: "Kristianstad Teater",
     year: "2016",
+    about:
+      "Astrid Lindgrens tidlösa historia om kärlek och mod! Berättelse handlar om lojalitet och motstånd, Brödrakärlek och konsten att våga fast man inte vågar!",
   },
   {
     id: "7",
@@ -65,14 +78,18 @@ export const workData: Work[] = [
     imageUrl: stageImages.ikea2.src,
     workplace: "Teater Västernorrland",
     year: "2016",
+    about:
+      "Föreställningen var en fartfylld och hyllad samtidssatir om Ingvar Kamprad och IKEA, där ensemblen spelade instrument och dansade, vilket markerade återinvigningen av teatern",
   },
   {
     id: "8",
     name: "I love you, you're perfect, now change!",
     role: "Kapellmästare, pianist",
-    imageUrl: "",
+    imageUrl: stageImages.loveyou.src,
     workplace: "Lilla Beddinge Teater",
     year: "2016",
+    about:
+      "Föreställningen skildrar kärlek och relationer genom 23 olika karaktärer, från dejting till pensionärslivet, med en stor ensemble som möjliggjorde unika produktionsnummer",
   },
   {
     id: "9",
@@ -81,6 +98,8 @@ export const workData: Work[] = [
     imageUrl: stageImages.zjivago.src,
     workplace: "Malmö Opera",
     year: "2014-2015",
+    about:
+      "Musikalen Doktor Zjivago, baserad på Boris Pasternaks klassiska roman och med musik av Lucy Simon, handlar om fyra ungdomars uppväxt och liv i revolutionens Ryssland och den unga Sovjetstaten under tidigt 1900-tal",
   },
   {
     id: "10",
@@ -89,5 +108,7 @@ export const workData: Work[] = [
     imageUrl: stageImages.malmo.src,
     workplace: "Malmö Opera",
     year: "2013-2014",
+    about:
+      "Handlingen i Miss Saigon startar i slutskedet av den amerikanska ockupationen där den unge amerikanske soldaten Chris och Kim, sjuttonårig vietnamesisk barflicka, blir förälskade i varandra. När Saigon faller överger han henne.",
   },
 ];
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -14,15 +14,10 @@ export default function Work() {
           >
             <p className="text-xl italic">{item.year}</p>
             <div className="flex flex-col align-center w-auto">
-              <p className="font-normal mb-3 md:text-xl">
+              <p className="font-semibold mb-3 md:text-xl">
                 {item.workplace} | {item.name}
               </p>
-              <p className="hidden md:block">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi
-                maxime ullam quasi ipsum facilis provident, praesentium
-                doloremque modi asperiores? A, est? Suscipit, dolor fuga ipsum
-                omnis at velit culpa vel?
-              </p>
+              <p className="hidden md:block">{item.about}</p>
             </div>
             <div className="md:flex md:justify-center hidden md:block">
               <img
@@ -30,6 +25,26 @@ export default function Work() {
                 src={item.imageUrl}
                 alt={item.name}
               />
+            </div>
+          </div>
+        ))}
+      </div>
+      <h3 className="text-2xl ml-10 mt-10 font-thin">TV/FILM</h3>
+      <div className="flex flex-col mr-10">
+        {work.map((item) => (
+          <div
+            className="grid grid-cols-2 my-6 ml-10 font-thin md:grid-cols-3"
+            key={""}
+          >
+            <p className="text-xl italic">ÅR</p>
+            <div className="flex flex-col align-center w-auto">
+              <p className="font-semibold mb-3 md:text-xl">
+                ARBETSPLATS | NAMN/TITEL
+              </p>
+              <p className="hidden md:block">OM</p>
+            </div>
+            <div className="md:flex md:justify-center hidden md:block">
+              <img className="w-80 rounded-md" src={""} alt={"ALTTEXT"} />
             </div>
           </div>
         ))}
