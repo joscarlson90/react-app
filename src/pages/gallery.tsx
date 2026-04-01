d4c8e51dfc16b1703ae39c2190e321e8bf9d3184
joscarlson90 <josefinecarlson90@gmail.com>
2026-04-01 13:06:38 +0200

    add second state for images

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { stageImages } from "../data/gallery";
 
 const hidden = [
@@ -6,22 +7,45 @@ const hidden = [
   stageImages.profil2,
   stageImages.young,
   stageImages.oliver2,
-  stageImages.zjivago
+  stageImages.zjivago,
+  stageImages.borje,
 ];
 
+type Image = {
+  id: number;
+  src: string;
+  description: string;
+  inGallery: boolean;
+};
+
 export default function Gallery({}) {
+  const [showDescription, setshowDescription] = useState<Image | null>(null);
+
   return (
-    <div className="columns-1 sm:columns-2 md:columns-3 gap-4 p-6">
-      {Object.values(stageImages)
-        .filter((img) => !hidden.includes(img))
-        .map((img) => (
-          <img
-            key={img.src}
-            src={img.src}
-            alt="Scenbilder"
-            className="mb-4 w-full hover:opacity-90 transition w-m h-auto rounded-md"
-          />
-        ))}
-    </div>
+    <>
+      <div className="relative z-50">
+        <div className="columns-1 sm:columns-2 md:columns-3 gap-4 p-6">
+          {Object.values(stageImages)
+            .filter((img) => !hidden.includes(img))
+            .map((img) => (
+              <div key={img.id} className="relative mb-4">
+                <img
+                  src={img.src}
+                  alt="Produktionsbilder"
+                  onClick={() =>
+                    setshowDescription(showDescription === img ? null : img)
+                  }
+                  className={showDescription === img ? "opacity-30" : ""}
+                />
+                {showDescription === img && (
+                  <div className="absolute transition inset-0 flex items-center justify-center text-black text-xl bg-opacity-20 rounded-md">
+                    <p>{showDescription.description}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
   );
 }
